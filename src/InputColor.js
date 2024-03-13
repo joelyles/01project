815c6d0497bf03307e3310e2459301e8997510c2
Joe Lyles <lylesjoe@protonmail.com>
2024-03-13 05:07:47 -0400

    practiced InputColor

--- src/InputColor.js
+++ src/InputColor.js
@@ -1,28 +1,21 @@
-import { useRef } from 'react';
+import { useRef } from "react";
 
-const InputColor = ({ colorValue, setColorValue, isDarkText, setIsDarkText }) => {
+const InputColor = ({ colorValue, setColorValue }) => {
     const inputRef = useRef();
-  return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <label>Add Color Name:</label>
-      <input 
-        autoFocus
-        ref={inputRef}
-        id="search"
-        type="text"
-        role="colorinput"
-        placeholder="add color name"
-        required
-        value={colorValue}
-        onChange={(e) => setColorValue(e.target.value)}
-         />
-        {/*  <button
-            type='button'
-            onClick={() => setIsDarkText(!isDarkText)}
-        > toggle text color
-        </button> */}
-    </form>
-  )
+
+    return (
+        <form>
+            <input 
+                type="text"
+                ref={inputRef}
+                name="color input"
+                id="color input"
+                placeholder="type color here"
+                value={ colorValue }
+                onChange={(e) => setColorValue(e.target.value) }
+            />
+        </form>
+    )
 }
 
 export default InputColor
